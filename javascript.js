4f60daeecb99c7f913029c99f5d08b41c8091406
BonkersCoding <195286253+BonkersCoding@users.noreply.github.com>
2025-02-28 23:43:46 +0100

    Make rainbow mode functional, add clear button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("#gridSize");
-const pixel = document.querySelectorAll(".pixel");
+let pixels = document.querySelectorAll(".pixel");
+const modes = document.querySelector(".modes");
+const clear = document.querySelector("#clear");
 let mode = "default";
-const modes = document.querySelector(".modes")
 let pixelNumber = 20;
 
 function generatePixels(number) {
@@ -17,7 +18,7 @@ function generatePixels(number) {
         
         for (let j = 0; j < number; j++) {    
             const pixel = document.createElement("div");
-            pixel.classList.add("pixel");
+            pixel.id = "pixel";
             row.appendChild(pixel);
         }
         container.appendChild(row);
@@ -40,11 +41,30 @@ btn.addEventListener('click', () => {
 
 container.addEventListener('mouseover', (event) => {
     let targetPixel = event.target;
-    targetPixel.classList.add("active");
+    if (targetPixel.id === "pixel") {
+        switch (mode) {
+            case "default":
+                targetPixel.style.backgroundColor = "black";
+                break;
+            case "rainbow":
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                targetPixel.style.backgroundColor = "#" + randomColor;
+                break;
+            case "darken":
+                
+            default:
+                break;
+        }
+}
 })
+
+clear.addEventListener('click', () => {changeGrid(pixelNumber)});
 
 modes.addEventListener('click', (event) => {
     let option = event.target;
     mode = option.id;
+    if (mode === "darken") {
+        pixels.forEach((pixel) => {pixel.style.backgroundColor = "black"});
+    }
     console.log(mode);
 })
