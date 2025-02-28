cc740cc3b7dde7dc244847872272e219d2d84e67
BonkersCoding <195286253+BonkersCoding@users.noreply.github.com>
2025-02-28 21:17:51 +0100

    Add changeGrid function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container")
+const btn = document.querySelector("#btn")
 
 function generatePixels(number) {
     for (let i = 0; i < number; i++) {
@@ -13,11 +14,16 @@ function generatePixels(number) {
         for (let j = 0; j < number; j++) {    
             const pixel = document.createElement("div");
             pixel.classList.add("pixel");
-            pixel.style.minHeight = "1px";
-            pixel.style.minWidth = "1px";
-            pixel.style.border = "1px solid black"
             row.appendChild(pixel);
         }
         container.appendChild(row);
     }
+}
+
+generatePixels(16);
+
+function changeGrid(number) {
+    let rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {container.removeChild(row)});
+    generatePixels(number);
 }
