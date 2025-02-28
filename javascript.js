a03d3ac487fd4c441dde063d23c4bd1d9572cdcb
BonkersCoding <195286253+BonkersCoding@users.noreply.github.com>
2025-02-28 20:02:06 +0100

    Set pixels into a square grid

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 const container = document.querySelector(".container")
 
 function generatePixels(number) {
-    const square = number * number;
-    for (let i = 0; i < square; i++) {    
+    for (let i = 0; i < number; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+    
         if (number < 1 || number > 100) {
             alert("Use a positive number, smaller than 100")
             break;
         }
-        const pixel = document.createElement("div");
-        pixel.style.height = "50px";
-        pixel.style.width = "50px";
-        container.appendChild(pixel);
+        
+        for (let j = 0; j < number; j++) {    
+            const pixel = document.createElement("div");
+            pixel.style.height = "50px";
+            pixel.style.width = "50px";
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
     }
 }
